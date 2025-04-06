f82ccb44025231daebdd6ac66825aa92b4a2a8a7
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-04-06 00:56:25 -0400

    feat: listen manifest changes

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::path::PathBuf;
 
 use clap::{Arg, Command};
@@ -7,7 +8,7 @@ use serde::Deserialize;
 pub struct WebhookConfig {
     pub path: Option<String>,
     pub secret: Option<String>,
-    pub events: Vec<String>,
+    pub events: HashSet<String>,
     pub shell: Option<Vec<String>>,
     pub command: Option<String>,
     pub script: Option<PathBuf>,
@@ -21,14 +22,85 @@ pub struct Config {
     pub webhooks: Vec<WebhookConfig>,
 }
 
-pub fn get_config() -> Config {
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            port: 8080,
+            verbose: "info".to_string(),
+            webhooks: Vec::new(),
+        }
+    }
+}
+
+impl Config {
+    pub fn merge(&mut self, other: Config) {
+        // merge the two webhooks list without repeating the same path
+        // in case of conflict, merge the events
+        for other_webhook in other.webhooks {
+            if let Some(index) = self
+                .webhooks
+                .iter()
+                .position(|wh| wh.path == other_webhook.path)
+            {
+                let mut existing_webhook = self.webhooks.remove(index);
+                existing_webhook.events.extend(other_webhook.events);
+                self.webhooks.push(existing_webhook);
+            } else {
+                self.webhooks.push(other_webhook);
+            }
+        }
+    }
+
+    pub fn print_paths(&self) {
+        for webhook in &self.webhooks {
+            println!("Webhook path: {}", webhook.path);
+            println!(
+                "\tEvents: {}",
+                webhook
+                    .events
+                    .iter()
+                    .cloned()
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
+    }
+}
+
+pub fn parse_config(path: &PathBuf) -> Config {
+    if path.is_dir() {
+        let mut config = Config::default();
+        for entry in std::fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            if entry.path().is_file() {
+                let cfg_content = std::fs::read_to_string(entry.path()).unwrap();
+                config.merge(
+                    toml::from_str(&cfg_content)
+                        .or_else(|_| serde_yaml::from_str(&cfg_content))
+                        .or_else(|_| serde_json::from_str(&cfg_content))
+                        .unwrap_or_default(),
+                );
+            }
+        }
+        config
+    } else {
+        let cfg_content = std::fs::read_to_string(path).unwrap();
+
+        toml::from_str(&cfg_content)
+            .or_else(|_| serde_yaml::from_str(&cfg_content))
+            .or_else(|_| serde_json::from_str(&cfg_content))
+            .unwrap_or_default()
+    }
+}
+
+pub fn get_config() -> (PathBuf, Config) {
     let args = Command::new("grhooks")
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
-            Arg::new("manifest")
-                .alias("config")
-                .env("GRHOOKS_MANIFEST")
+            Arg::new("manifest-dir")
+                .alias("config-dir")
+                .env("GRHOOKS_MANIFEST_DIR")
                 .required(true)
                 .num_args(1)
                 .help("Path to the configuration file")
@@ -46,20 +118,14 @@ pub fn get_config() -> Config {
         .get_matches();
 
     let config_path = args
-        .get_one::<PathBuf>("manifest")
+        .get_one::<PathBuf>("manifest-dir")
         .expect("No config file provided");
 
     let verbose =
         std::env::var("GRHOOKS_LOG").unwrap_or_else(|_| args.get_count("verbose").to_string());
 
     println!("Reading configs from path: {config_path:?}");
+    let config = parse_config(config_path);
 
-    let cfg_content = std::fs::read_to_string(&config_path).unwrap();
-
-    let config = toml::from_str(&cfg_content)
-        .or_else(|_| serde_yaml::from_str(&cfg_content))
-        .or_else(|_| serde_json::from_str(&cfg_content))
-        .expect("Failed to parse config");
-
-    Config { verbose, ..config }
+    (config_path.clone(), Config { verbose, ..config })
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::sync::Arc;
 
 use axum::{Router, routing::post};
 use grhooks_config::Config;
+use notify::event::{DataChange, ModifyKind};
+use notify::{EventHandler, EventKind, Watcher};
 use tokio::sync::RwLock;
 use tracing::level_filters::LevelFilter;
 
@@ -13,17 +15,24 @@ pub(crate) type GlobalConfig = Arc<RwLock<Config>>;
 
 #[tokio::main]
 async fn main() {
-    let config = grhooks_config::get_config();
+    let (config_path, config) = grhooks_config::get_config();
     tracing_subscriber::fmt()
         .with_max_level(LevelFilter::from_str(&config.verbose).unwrap_or(LevelFilter::INFO))
         .with_file(true)
         .with_line_number(true)
         .init();
+    config.print_paths();
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.port))
         .await
         .unwrap();
     let state = Arc::new(RwLock::new(config));
+
+    let mut manifest_watcher = notify::recommended_watcher(listen_config_changes(state.clone()))
+        .expect("Cannot create watcher for manifest");
+    manifest_watcher
+        .watch(config_path.as_path(), notify::RecursiveMode::Recursive)
+        .unwrap();
 
     let app = Router::new()
         .route("/{*path}", post(handlers::webhook_handler))
@@ -37,4 +46,22 @@ async fn main() {
     println!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap()
+}
+
+fn listen_config_changes(state: GlobalConfig) -> impl EventHandler {
+    move |res: notify::Result<notify::Event>| {
+        let Ok(event) = res else {
+            tracing::error!("Error watching config file: {}", res.unwrap_err());
+            return;
+        };
+        if event.kind == EventKind::Modify(ModifyKind::Data(DataChange::Any)) {
+            let mut config = state.blocking_write();
+            config.webhooks = Vec::new(); // restore webhooks
+            event.paths.iter().for_each(|path| {
+                tracing::info!("Config file changed: {path:?}");
+                config.merge(grhooks_config::parse_config(&path))
+            });
+            config.print_paths();
+        }
+    }
 }
