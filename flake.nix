ddb71050c669388374d77afac23d4a932e60b9d3
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-06-08 23:20:57 -0400

    fix: remove manifest from push docker image

--- flake.nix
+++ flake.nix
@@ -49,14 +49,13 @@
           { arch = "aarch64"; os = "linux"; target = "aarch64-unknown-linux-gnu"; formats = ["deb" "rpm" "tar.xz"]; }
           { arch = "armv7l"; os = "linux"; target = "armv7-unknown-linux-gnueabihf"; formats = ["tar.xz"]; }
           { arch = "armv6l"; os = "linux"; target = "arm-unknown-linux-gnueabihf"; formats = ["tar.xz"]; }
-          { arch = "riscv32"; os = "linux"; target = "riscv32gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
-          { arch = "riscv64"; os = "linux"; target = "riscv64gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
+          # { arch = "riscv32"; os = "linux"; target = "riscv32gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
+          # { arch = "riscv64"; os = "linux"; target = "riscv64gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
           # MacOS
           { arch = "x86_64"; os = "macos"; target = "x86_64-apple-darwin"; formats = ["tar.xz"]; }
           { arch = "aarch64"; os = "macos"; target = "aarch64-apple-darwin"; formats = ["tar.xz"]; }
           # Windows
           { arch = "x86_64"; os = "windows"; target = "x86_64-pc-windows-msvc"; formats = ["zip"]; }
-          { arch = "i686"; os = "windows"; target = "i686-pc-windows-msvc"; formats = ["zip"]; }
         ];
 
         mkDevShell = { arch, os, ... }: pkgs.mkShell {
@@ -85,12 +84,18 @@
 
         containerPkg = variant: let
           pkg = mkPackage variant;
-          # arch = if variant.arch == "x86_64" then "amd" else "arm";
+          dockerPlatform =
+            if variant.arch == "x86_64" then "amd64"
+            else if variant.arch == "aarch64" then "arm64"
+            else if variant.arch == "armv7l" then "arm"
+            else if variant.arch == "armv6l" then "arm"
+            else if variant.arch == "i686" then "386"
+            else throw "Unsupported arch: ${variant.arch}";
         in pkgs.dockerTools.buildLayeredImage rec {
           name = cargoManifest.package.name;
           tag = cargoManifest.package.version;
           created = "now";
-          # architecture = "linux/${arch}64";
+          architecture = dockerPlatform;
 
           contents = [ pkg ];
           config.Cmd = ["/bin/${name}"];
