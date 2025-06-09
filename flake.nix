9c2833f3a26a334a84c2df3c3bb70f5a2d8cb8b6
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-06-08 23:49:51 -0400

    refactor: move architectures to other file

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     flake-utils.lib.eachDefaultSystem (baseSystem:
       let
         cargoManifest = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        architectures = import ./architectures.nix;
+
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           system = baseSystem;
@@ -42,21 +44,6 @@
           crossSystem = if cross != "x86_64-linux" then crossSystem else null;
           localSystem = baseSystem;
         };
-
-        architectures = [
-          # Linux
-          { arch = "x86_64"; os = "linux"; target = "x86_64-unknown-linux-gnu"; formats = ["deb" "rpm" "tar.xz"]; }
-          { arch = "aarch64"; os = "linux"; target = "aarch64-unknown-linux-gnu"; formats = ["deb" "rpm" "tar.xz"]; }
-          { arch = "armv7l"; os = "linux"; target = "armv7-unknown-linux-gnueabihf"; formats = ["tar.xz"]; }
-          { arch = "armv6l"; os = "linux"; target = "arm-unknown-linux-gnueabihf"; formats = ["tar.xz"]; }
-          # { arch = "riscv32"; os = "linux"; target = "riscv32gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
-          # { arch = "riscv64"; os = "linux"; target = "riscv64gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
-          # MacOS
-          { arch = "x86_64"; os = "macos"; target = "x86_64-apple-darwin"; formats = ["tar.xz"]; }
-          { arch = "aarch64"; os = "macos"; target = "aarch64-apple-darwin"; formats = ["tar.xz"]; }
-          # Windows
-          { arch = "x86_64"; os = "windows"; target = "x86_64-pc-windows-msvc"; formats = ["zip"]; }
-        ];
 
         mkDevShell = { arch, os, ... }: pkgs.mkShell {
           packages = with pkgs; [ rustc cargo ];
